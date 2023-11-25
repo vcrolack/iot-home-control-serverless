655018d838e92685e00729c6c0c17f08e66730b6
Victor Rolack <victorolack@gmail.com>
2023-11-25 20:00:27 -0300

    add authorization on get users by id and get pantries by id

--- src/lambdas/users/getUsersById/index.js
+++ src/lambdas/users/getUsersById/index.js
@@ -1,12 +1,17 @@
-const executeQuery = require("../../../db/dbManager");
-const { errorHandler } = require("../../../helpers/errorHandler");
-const { GET_USER_BY_ID } = require("../../../queries/userQueries");
-
-
+const executeQuery = require('../../../db/dbManager');
+const { errorHandler } = require('../../../helpers/errorHandler');
+const { GET_USER_BY_ID } = require('../../../queries/userQueries');
+const {verifyRole} = require('../../../helpers/verifyRole');
+const { adminTest } = require('../../../../config/roles');
 
 const getUsersById = async (event, context) => {
   try {
     const { id } = event.pathParameters;
+    
+    if (!event.headers.Authorization) return errorHandler({statusCode: 400, message: 'Bad request'});
+
+    const decodedToken = verifyRole(event.headers.Authorization, adminTest);
+    if (decodedToken.statusCode) return decodedToken;
 
     const result = await executeQuery(GET_USER_BY_ID, [parseInt(id)]);
 
